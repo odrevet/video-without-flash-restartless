4bba5d462b27d91b48dc88b40009a131f1d6811b
Olivier Drevet <odrevet@gmail.com>
2014-04-16 20:10:47 +0200

    added default preferences

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,20 @@
 const {interfaces: Ci, utils: Cu} = Components;
 Cu.import('resource://gre/modules/Services.jsm');
 
+const PREF_BRANCH = "extensions.vwof.";
+const PREFS = {
+    "modules":'{"HTML5":1, "springboard":1, "blip":1, "youtube":1, "dew":1, "SWM":1, "dailymotion":1, "ustream":1, "niconico":1, "FC2":1}',
+    "prefered_quality":'medium',
+    "prefered_format":'webm'
+};
+
+function setDefaultPrefs() {
+    let branch = Services.prefs.getDefaultBranch(PREF_BRANCH);
+    for (let [key, val] in Iterator(PREFS)) {
+        branch.setCharPref(key, val);
+    }
+}
+
 function listenPageLoad(event) {
     var cw = event.originalTarget.defaultView;
     if (cw.frameElement && windowListener.ignoreFrames) {
@@ -10,6 +24,7 @@ function listenPageLoad(event) {
 }
 
 function startup(aData, aReason) {
+    setDefaultPrefs();    
     Services.scriptloader.loadSubScript('chrome://vwof/content/vwof.js');    
     Services.scriptloader.loadSubScript('chrome://vwof/content/player.js');
     Services.scriptloader.loadSubScript('chrome://vwof/content/utils.js');
--- defaults/preferences/vwof.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.vwof.activate_onload", true);
-pref("extensions.vwof.prefered_quality", 'medium');
-pref("extensions.vwof.prefered_format", 'webm');
-pref("extensions.vwof.modules", '{"HTML5":1, "springboard":1, "blip":1, "youtube":1, "dew":1, "SWM":1, "dailymotion":1, "ustream":1, "niconico":1, "FC2":1, "twitch":1}');
